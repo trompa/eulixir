fa3a5159290e5ac3d987e9d76e3e985fc98b7d48
Administrator <alberto@trompa.eu>
2014-06-08 13:02:41 +0200

    use of Integer module

--- Problem2.exs
+++ Problem2.exs
@@ -1,5 +1,7 @@
+require Integer
+
 Stream.unfold( {0,1}, fn {x1,x2} -> { x1,{x2, x1+x2} } end) 
 |> Enum.take_while( &(&1 < 4_000_000) ) 
-|> Enum.filter( &( rem(&1,2) == 0) ) 
+|> Enum.filter( &Integer.even?/1 ) 
 |> Enum.sum
-|> Io.puts
+|> IO.puts
